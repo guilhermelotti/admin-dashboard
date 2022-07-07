050e12c3eea69b9ceb3bdaf33670ca8d80159836
Guilherme Lotti <guilherme.lotti@outlook.com>
2022-07-07 11:01:13 -0300

    feat: add initial theme/font + global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { Sidebar } from "./components/Sidebar";
+import GlobalStyles from "./styles/global";
+import { theme } from "./styles/theme";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Sidebar />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
